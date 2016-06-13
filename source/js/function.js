573cacbfa982c1948b937938a7b1837287d9a89e
vinod soba <v.soba@registry-trust.org.uk>
2016-06-13 16:12:04 +0100

    New changes made to files 13/06/2016 16:11

--- source/js/function.js
+++ source/js/function.js
@@ -1,155 +1,50 @@
-
-
-var title = "Vinod Soba";
-
-var canvas = document.getElementById("canvas");
-var g2d = canvas.getContext("2d");
-
-
-g2d.fillStyle = "black";
-g2d.fillRect(100,100,200,200);
-g2d.fillText(title, canvas.width / 2, canvas.width, 120);
-g2d.font = "30px Comic Sans MS";
-
-
 var vinny = document.getElementById("vinny-container");
-clearInterval(vinny);
+var intro = document.getElementById("introduction");
+var isVinnyStatic = true;
 
 
-var gName;
-var g2d;
-var width = 1900;
-var height = 800;
-var title = "Welcome to my Website Vinod Soba";
-var title2 = "1.Resume";
-var title3 = "2.Portfolio";
 var count =0;
 var x;
 var y;
 var framesPerSecond = 2;
 var currentPos =0;
-var MyImage = new Image();
+var w = window.innerWidth;
+var h = window.innerHeight;
 
-/*
-var dy = 400;
-var dx = 780;
-var dw = 235;
-var dh = 200;
-var sx = 10;
-var sy = 0;
-var sw = 215;
-var sh = 190;
 
-*/
+
 //animate the sprite
 
-var gameState = 0;
-
 window.onload = function(){
-	init();	
-	scroll();
+	
+	getWindow();
 }
 
+function getWindow() {  
 
-function init(){
-	gName = document.getElementById("canvas");
-	gName.width = width;
-	gName.height = height;
-
-	g2d = gName.getContext("2d");
-	g2d.imageSmoothingEnabled = false;
-	g2d.webkitImageSmoothingEnabled = false;
-	g2d.mozImageSmoothingEnabled = false;
-
-	console.log("Game initialised");
-	draw();
+   
+    
 }
-
-function draw(){
-	if(gameState == 0){
-		g2d.font = "40px Segoe ui";
-		// Create gradient
-		var gradient=g2d.createLinearGradient(0,0,gName.width,0);
-		gradient.addColorStop("0.5","#CDCFD1");
-		gradient.addColorStop("1.0","#8C8E90");
-		g2d.fillStyle = gradient;
-		g2d.fillText(title, (width / 2) - (g2d.measureText(title).width / 2), 150);		
-		
-
-		
-		
-
-		g2d.font = "30px Segoe ui";
-		// Create gradient
-		var gradient=g2d.createLinearGradient(0,0,gName.width,0);
-		gradient.addColorStop("0.5","#CDCFD1");
-		gradient.addColorStop("1.0","#8C8E90");
-		g2d.fillStyle = gradient;
-		g2d.fillText(title2, (width / 2) - (g2d.measureText(title2).width / 2), 190);
-		g2d.fillText(title3, (width / 2) - (g2d.measureText(title3).width / 2), 230);
-	}
-
-
-	if(gameState == 1){
-		
-		g2d.font = "30px Segoe ui";
-		// Create gradient
-		var gradient=g2d.createLinearGradient(0,0,gName.width,0);
-		gradient.addColorStop("0.5","#CDCFD1");
-		gradient.addColorStop("1.0","#8C8E90");
-		g2d.fillStyle = gradient;
-		g2d.fillText("Interactive Resume", (width / 2) - (g2d.measureText("Interactive Resume").width / 2), 150);
-		
-	}
-
-}
-
-function drawPath(){
-		
-		MyImage.src = "images/vinny-slides.png";
-		
-		setTimeout(function(){ 
-
-		
-		g2d.clearRect(790, 300, 215,190);
-
-		x = (count % 2) * 215;
-		y = Math.floor(count / 7) * 190;
-		g2d.drawImage(MyImage, x, y, 215, 190, 790, 300, 215, 190);
-
-		if(count == 7){
-			count = 0;
-		}else{
-			count++;
-		}
-		console.log(y);
-	}, 5000 / framesPerSecond);
-
-		
-
-}
-
-requestAnimationFrame(drawPath);
-
-
 
 
 
 function onScrollEventHandler(ev)
-    {
-    	
-        drawPath();
-        //http://dev.w3.org/2006/webapi/DOM-Level-3-Events/html/DOM3-Events.html#event-type-scroll
-    } 
+ {
+    if (w >= 1925){
+   		scroll();   	
+    }
+    else{ alert("Need to set value");	 }
+   console.log(innerHTML = "Width: " + w + "<br>Height: " + h);
+    //http://dev.w3.org/2006/webapi/DOM-Level-3-Events/html/DOM3-Events.html#event-type-scroll
+} 
 
 
+var el=window;
 
-    var el=window;
-
-    if(el.addEventListener)
-        el.addEventListener('scroll', onScrollEventHandler, false);   
-    else if (el.attachEvent)
-        el.attachEvent('onscroll', onScrollEventHandler); 
+if(el.addEventListener)
+     el.addEventListener('scroll', onScrollEventHandler, false);   
+else if (el.attachEvent)
+     el.attachEvent('onscroll', onScrollEventHandler); 
 
 function scroll(){
 	var lastScroll = 0;
